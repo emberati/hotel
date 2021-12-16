904badabe2e7ae6c97e1d910f923525f2f1049ea
emberati <alex.shak55@gmail.com>
2021-12-16 20:00:17 +0300

    Удалён ifmain в app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 from app import routes, models
